8b6f1426028f681dc4c13fd60b1c68331fec21e0
CarlJohan12 <carljohan.eklund.2@gmail.com>
2025-03-03 18:03:07 +0100

    Added db info to file

--- backend/activeTrip.js
+++ backend/activeTrip.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-
+require('dotenv').config();
 
 router.get('/active-trip-status', async (req, res) => {
     try {
